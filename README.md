2f1f4a72cce917288df1625a3ec5ed50cbf4b50d
Chukwuma Udele <106890540+chukseminence@users.noreply.github.com>
2025-03-03 02:03:43 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# fitnes
+# apex-fitness-website
---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+'use strict';
+
+
+
+
+
+/**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+
+
+//Remove the pre-loader when the page has loaded
+window.addEventListener("load", () => {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+  
+})
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () { navbar.classList.toggle("active"); }
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () { navbar.classList.remove("active"); }
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
